c1672131a7b13da14d13a42877668e45f3404ada
Tomasz Dudzic <42539821+paluch11998866@users.noreply.github.com>
2020-09-06 15:18:07 +0200

    Update PythonGmailMailRemover.py

--- PythonGmailMailRemover.py
+++ PythonGmailMailRemover.py
@@ -15,6 +15,7 @@ for x in range(emails):
     elem = driver.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[7]/div/div[1]/div[3]/div/table/tbody/tr[1]/td[5]/div[1]/div/div")
     elem.click()
     sleep(2)
+    #try to get email address from mail not delivered error and print it on the console.
     try:
         elem2 = driver.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td[1]/div[2]/div[2]/div/div[3]/div[3]/div/div/div/div/div[1]/div[2]/div[3]/div[3]/div/div[2]/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr[2]/td/a")
         print(elem2.text)
